931eec035ca1378b988c601d8cbb6a8721d65f0a
CaioACN <60967502+CaioACN@users.noreply.github.com>
2021-11-29 20:19:25 -0300

    atualização da

--- src/app/views/components/template/home/home.component.ts
+++ src/app/views/components/template/home/home.component.ts
@@ -12,8 +12,15 @@ export class HomeComponent implements OnInit {
   constructor(public dialog:MatDialog) { }
 
   ngOnInit(): void {
+    window.onload = function() {
+      if(!window.location.hash) {
+          this.window.location = window.location + '#carregar';
+          window.location.reload();
+      }
+  }
   }
 
+ 
 
   openDialog(){
     const dialogRef = this.dialog.open(HomeDialogComponent);
